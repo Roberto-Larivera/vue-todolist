6a364163aeb674797741bd59417e352623a7dbcd
Roberto Larivera <robylariv@gmail.com>
2023-01-24 14:25:00 +0100

    Inizializzazione progetto

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+console.log('int ok');
+const {createApp} = Vue;
+
+createApp ({
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+}).mount('#app')
