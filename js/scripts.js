cd87a1df0a5d235df81fd4caadc0a6492809ba17
Roberto Larivera <robylariv@gmail.com>
2023-01-24 15:33:19 +0100

    Completato to do list base

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,18 @@ createApp ({
                 'Fare i piatti',
                 'Fare il letto',
                 'Andare al commercialista'
-            ]
+            ],
+            newTask : null,
         }
     },
     methods: {
-        
+        removeListTask (index){
+            this.listTask.splice(index,1)
+        },
+        pushListTask (text){
+            this.listTask.push(text);
+            this.newTask = null;
+        },
     },
     mounted() {
         
